97f0a11956b852d4f9bf2ec6ca32fa40445e17d3
Susan Boyd <sueboyd922@gmail.com>
2022-05-17 17:05:17 -0400

    Test: Items API w/o serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/api/v1/merchants", to: "api/v1/merchants#index"
+  get "/api/v1/items", to: "api/v1/items#index"
 end
--- spec/requests/api/v1/items_requests_spec.rb
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -11,6 +11,21 @@ RSpec.describe 'Items API' do
       items = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
+      expect(items.count).to eq(5)
+
+      items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_an Integer
+
+        expect(item).to have_key(:description)
+        expect(item[:description]).to be_a String
+
+        expect(item).to have_key(:unit_price)
+        expect(item[:unit_price]).to be_a Float
+
+        expect(item).to have_key(:merchant_id)
+        expect(item[:merchant_id]).to be_a Integer
+      end
     end
   end
 end
