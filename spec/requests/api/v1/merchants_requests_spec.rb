c1a24ccff2701b68ac74fa92535e49308212652f
Susan Boyd <sueboyd922@gmail.com>
2022-05-17 18:24:03 -0400

    Test: Test for api/v1/merchants/:id

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe "Merchants API" do
       end
     end
   end
+
+  describe 'one merchant' do
+    it 'gets the info from one merchant' do
+      merchant = create_list(:merchant, 1).first
+
+      get "/api/v1/merchants/#{merchant.id}"
+
+      expect(response).to be_successful
+      merchant_response = JSON.parse(response.body, symbolize_names: true)
+      merchant = merchant_response[:data]
+
+      expect(mercahnt.count).to eq(1)
+      expect(merchant).to be_a Hash
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a String
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a String
+      expect(merchant[:type]).to eq("merchant")
+    end
+  end
 end
