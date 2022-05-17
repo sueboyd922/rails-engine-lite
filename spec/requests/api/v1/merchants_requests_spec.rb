af3faf6ad6bc1827164707a5f8ae73e9ba6121e6
Susan Boyd <sueboyd922@gmail.com>
2022-05-17 16:09:57 -0400

    Test: Get All Merchants Testing w/o serializer

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -8,6 +8,18 @@ RSpec.describe "Merchants API" do
       get '/api/v1/merchants'
 
       expect(response).to be_successful
+      # require "pry"; binding.pry
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(merchants.count).to eq(5)
+
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an Integer
+
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a String
+      end
+
     end
   end
 end
