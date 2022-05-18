be8b9e3ca82aa3e33ed070e54342a32df1453c03
Susan Boyd <sueboyd922@gmail.com>
2022-05-18 09:49:14 -0400

    ap1/v1/items/:id endpoint passing

--- spec/requests/api/v1/items_requests_spec.rb
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -48,14 +48,19 @@ RSpec.describe 'Items API' do
       expect(item).to be_a Hash
       expect(item).to have_key(:id)
       expect(item[:id]).to be_a String
+
       expect(item).to have_key(:type)
       expect(item[:type]).to eq("item")
+
       expect(item[:attributes]).to have_key(:name)
       expect(item[:attributes][:name]).to be_a String
+
       expect(item[:attributes]).to have_key(:description)
       expect(item[:attributes][:description]).to be_a String
+
       expect(item[:attributes]).to have_key(:unit_price)
       expect(item[:attributes][:unit_price]).to be_a Float
+
       expect(item[:attributes]).to have_key(:merchant_id)
       expect(item[:attributes][:merchant_id]).to be_an Integer
     end
