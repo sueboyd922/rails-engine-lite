2650e50925b577994fc131133c4deb6eb2d0e510
Susan Boyd <sueboyd922@gmail.com>
2022-05-26 15:51:25 -0400

    Set up relationships in models

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices
+  has_many :invoice_items, through: :items
+  has_many :transactions, through: :invoices
 
   def self.find_by_name(search)
     where("name ilike ?", "%#{search}%")
